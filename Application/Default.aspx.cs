e6cea189faf11cb3e674fb6f942d3d689f83ae97
Ilia Sorokin <cloud0beats@gmail.com>
2024-11-16 00:34:49 -0800

    Comibed feauters

--- Application/Default.aspx.cs
+++ Application/Default.aspx.cs
@@ -44,7 +44,7 @@ namespace Application
             }
             else
             {
-                Response.Redirect("Staff.aspx");
+                Response.Redirect("Admin.aspx");
             }
         }
     }
